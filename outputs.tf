13eda3093dd0d5186d97edcdb79f519cc3f1e69f
tiandaoburen <97828377+tiandaoburen@users.noreply.github.com>
2023-02-04 19:14:41 +0300

    changed repository output

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "repositories" {
   description = "map of github repositories"
-  value       = module.repositories
+  value       = join(" ", keys(module.repositories))
   # it's not sensitive, I just don't want to clutter logs
   sensitive = true
 }
