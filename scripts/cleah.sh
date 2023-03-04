2f19df255ac48e95a73d03f31b784567bfc32d7a
tiandaoburen <97828377+tiandaoburen@users.noreply.github.com>
2023-03-04 16:14:52 +0300

    chore(Makefile): create Makefile
    Signed-off-by: tiandaoburen <97828377+tiandaoburen@users.noreply.github.com>

--- scripts/cleah.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -Eeuxo pipefail
-
-read -ra args < <(terraform state list)
-terraform state rm "${args[@]}"
--- scripts/docs.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -Eeuxo pipefail
-
-terraform-docs markdown table --recursive --output-file README.md .
